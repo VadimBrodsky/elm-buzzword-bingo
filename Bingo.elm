d5548808297f0464c90dea226b6c0d3d488de831
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-16 22:43:11 -0500

    Add Html elements for the page and attributes

--- Bingo.elm
+++ Bingo.elm
@@ -1,16 +1,33 @@
 module Bingo where
 
-import Html
-import String
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import String exposing (toUpper, repeat, trimRight)
 
 title message times =
   message ++ " "
-    |> String.toUpper
-    |> String.repeat times
-    |> String.trimRight
-    |> Html.text
+    |> toUpper
+    |> repeat times
+    |> trimRight
+    |> text
+
+
+pageHeader =
+  h1 [ ] [ title "bingo!" 3]
+
+
+pageFooter =
+  footer [ ]
+         [ a [ href "https://pragmaticstudio.com" ]
+             [ text "The Pragmatic Studio" ]
+         ]
+
+
+view =
+  div [ id "container" ] [ pageHeader, pageFooter ]
 
 
 main =
-  title "bingo!" 3
+  view
 
