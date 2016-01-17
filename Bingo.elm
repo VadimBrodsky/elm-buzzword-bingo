1ecc58bac576b81ee9a12c1e442a9c8889081fd1
Vadim Brodsky <vadim.brodsky@gmail.com>
2016-01-16 21:39:07 -0500

    Add whitespace

--- Bingo.elm
+++ Bingo.elm
@@ -4,8 +4,12 @@ import Html
 import String
 
 main =
+
+  Html.text String.toUpper
+
   -- Html.text (String.repeat 3 (String.toUpper "bingo!"))
-  "bingo!"
-    |> String.toUpper
-    |> String.repeat 3
-    |> Html.text
+
+  -- "bingo!"
+  --   |> String.toUpper
+  --   |> String.repeat 3
+  --   |> Html.text
